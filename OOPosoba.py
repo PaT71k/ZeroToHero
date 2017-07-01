06ca49314fc9d2dd43a4ed6d1f624bec5c715d71
PaT71k <pv_hr@hotmail.com>
2017-07-01 14:32:06 +0200

    blackjack

--- OOPosoba.py
+++ OOPosoba.py
@@ -3,10 +3,18 @@ class Osoba(object):
         self.ime = ime
         self.god = god
 
+    def sayname(self):
+        print('Dobar dan. Moje ime je {}'.format(self.ime))
+
+    def __str__(self):
+        return self.ime + self.god.__str__()
 
 markoOsoba = Osoba('Marko', 14)
 ivanOsoba = Osoba('Ivan', 18)
 
 print(ivanOsoba.god - markoOsoba.god)
 print (markoOsoba.ime, ivanOsoba.ime)
+
+markoOsoba.sayname()
+print (markoOsoba.__str__())
 
--- range.py
+++ range.py
@@ -3,3 +3,6 @@ b = range(8, 15)
 c = list(b)
 for n in b:
     print(n)
+
+for n in c:
+    print(n)
